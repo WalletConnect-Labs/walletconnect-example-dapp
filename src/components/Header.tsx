4861f9c9b8187a2ff40950ff265bcaa23d94c3ea
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-17 21:32:42 +0000

    refactor old example dapp

---
+++ src/components/Header.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import styled from "styled-components";
+import * as PropTypes from "prop-types";
+import Blockie from "./Blockie";
+import { ellipseAddress } from "../helpers/utilities";
+import banner from "../assets/walletconnect-banner.png";
+import { transitions } from "../styles";
+
+const SHeader = styled.div`
+  margin-top: -1px;
+  margin-bottom: 1px;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 16px;
+`;
+
+const SBannerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const SBanner = styled.div`
+  width: 275px;
+  height: 45px;
+  background: url(${banner}) no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
+
+const SActiveAccount = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  font-weight: 500;
+`;
+
+const SDisconnect = styled.div`
+  transition: ${transitions.button};
+  font-size: 12px;
+  font-family: monospace;
+  position: absolute;
+  right: 0;
+  top: 30px;
+  opacity: 0.7;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-1px);
+    opacity: 0.5;
+  }
+`;
+
+const SBockieWrapper = styled.div`
+  margin-right: 10px;
+  & canvas {
+    border-radius: 3px;
+  }
+`;
+
+const Header = (props: any) => {
+  const { killSession, address } = props;
+  return (
+    <SHeader {...props}>
+      <SBannerWrapper>
+        <SBanner />
+      </SBannerWrapper>
+      {address && (
+        <SActiveAccount>
+          <SBockieWrapper>
+            <Blockie address={address} />
+          </SBockieWrapper>
+          <p>{ellipseAddress(address)}</p>
+          <SDisconnect onClick={killSession}>{"Disconnect"}</SDisconnect>
+        </SActiveAccount>
+      )}
+    </SHeader>
+  );
+};
+
+Header.propTypes = {
+  killSession: PropTypes.func.isRequired,
+  address: PropTypes.string
+};
+
+export default Header;
